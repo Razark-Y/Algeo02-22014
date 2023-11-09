e7ae80586b37f4d06465f7075678d5d17b8876cb
Rafly Hanggaraksa <ajarafly422@gmail.com>
2023-11-09 22:18:18 +0700

    factor: basic structure for vue

--- src/vue-app/src/App.vue
+++ src/vue-app/src/App.vue
@@ -1,12 +1,21 @@
 <template>
-  <h1>Reverse Image Search</h1>
-  <div>
-    <img src="" alt="uploadedimage">
-    <div>
-      <!-- <form action="">
-        <input type="file" name="image" accept="image/*">
-      </form> -->
-
+  <div class="flex flex-col justify-center items-center">
+    <h1 class="font-bold">Reverse Image Search</h1>
+    <div class="flex gap-5">
+      <img src="" alt="uploadedimage">
+      <div >
+        <form action="">
+          <div class="flex flex-col">
+            <label for="image" class="bg-orange-500 px-3 py-1 rounded-md cursor-pointer">Image Input
+              <input type="file" id="image" name="image" accept="image/*" class="hidden">
+            </label>
+    
+            <label for="tipeCBIR">Color / Texture</label>
+            <input type="checkbox" name="tipeCBIR" id="">
+            <button>Search</button>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
 
